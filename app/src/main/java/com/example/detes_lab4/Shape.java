79b890c12fa7c934f7005cda128ecff76ba64796
Diana Le <dble@ualberta.ca>
2021-10-02 14:10:36 -0600

    triangle

--- app/src/main/java/com/example/detes_lab4/Shape.java
+++ app/src/main/java/com/example/detes_lab4/Shape.java
@@ -3,6 +3,7 @@ package com.example.detes_lab4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "pink";
 
     public Shape(int x, int y) {
         this.x = x;
