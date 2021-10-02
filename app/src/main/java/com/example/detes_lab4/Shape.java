d8e5b6bc321474baff614295404c34d779e0a28c
Derek T <58311797+dtrinh15@users.noreply.github.com>
2021-10-02 14:08:22 -0600

    changed color in shape

--- app/src/main/java/com/example/detes_lab4/Shape.java
+++ app/src/main/java/com/example/detes_lab4/Shape.java
@@ -3,7 +3,7 @@ package com.example.detes_lab4;
 public abstract class Shape {
     private int x;
     private int y;
-
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
